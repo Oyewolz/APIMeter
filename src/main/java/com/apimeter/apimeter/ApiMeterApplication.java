fdc2bcbe9abf87f3ec3d4d9b07eae7c22552f2fa
sfxtoyewole <toyewole@seamfix.com>
2023-05-20 11:31:53 +0100

    create project

---
+++ src/main/java/com/apimeter/apimeter/ApiMeterApplication.java
@@ -0,0 +1,13 @@
+package com.apimeter.apimeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apimeter/apimeter/ApiMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apimeter.apimeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
